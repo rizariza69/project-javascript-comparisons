42a426a204c3b44828f0ad67d65406d3ac576bac
Muhammad Riza <riza.riza69@gmail.com>
2019-04-07 17:42:15 +0700

    add readme and tes with function

--- index.js
+++ index.js
@@ -1,62 +1,79 @@
-"use strict"
+"use strict";
 
-const nameA = 'Alpha'
-const nameB = 'Beta'
-const nameCompared = nameA === nameB
+const nameA = "Alpha";
+const nameB = "Beta";
+const nameCompared = nameA === nameB;
 
-console.log(nameA)
-console.log(nameB)
-console.log(nameCompared)
+console.log(nameA);
+console.log(nameB);
+console.log(nameCompared);
 
 // Feel free to have some experiments with comparisons
 
-const ageA = 18
-const ageB = 25
+const ageA = 18;
+const ageB = 25;
 
 if (ageA > ageB) {
-  console.log('too old')
+  console.log("too old");
 } else if (ageA < ageB) {
-  console.log('too young')
+  console.log("too young");
 } else {
-  console.log('its your age')
+  console.log("its your age");
 }
 
 // Feel free to have some experiments with comparisons
 
 const personA = {
-  name: 'Alpha',
+  name: "Alpha",
   age: 20
-}
+};
 
 const personB = {
-  name: 'Betty',
+  name: "Betty",
   age: 30
-}
+};
 
 switch (personA.age > personB.age) {
   case true:
-    console.log(`${personA.name} more than youngwith ${personB.name}`)
-    break
+    console.log(`${personA.name} more than youngwith ${personB.name}`);
+    break;
 
   case false:
-    console.log(`${personB.name} more than older with ${personA.name}`)
-    break
+    console.log(`${personB.name} more than older with ${personA.name}`);
+    break;
 
   default:
-    console.log('good compare age')
+    console.log("good compare age");
 }
 
 // Feel free to have some experiments with comparisons
 
+let name = "domi";
+let job = "soldier";
 
-let name = 'domi';
-let job = 'soldier';
-
-
-if (name === '' && job === '') {
-  console.log('please create your name!!');
-} else if (name === 'domi' && job === '') {
+if (name === "" && job === "") {
+  console.log("please create your name!!");
+} else if (name === "domi" && job === "") {
   console.log(`helo ${name}, welcome to the world`);
-} else if (name === 'domi' && job === 'soldier') {
+} else if (name === "domi" && job === "soldier") {
   console.log(`Come on ${name} save the world`);
 }
+
+//tes with function
+
+function compareNumber(num1, num2) {
+  if (num1 < num2) {
+    return true;
+  } else if (num1 > num2) {
+    return false;
+  } else {
+    return -1;
+  }
+}
+
+// TEST CASES
+console.log(compareNumber(5, 8)); // true
+console.log(compareNumber(5, 3)); // false
+console.log(compareNumber(4, 4)); // -1
+console.log(compareNumber(3, 3)); // -1
+console.log(compareNumber(10, 2)); // false
